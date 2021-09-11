5607aa931946be54b7edf50e88348e8f5f8f472f
reginald mabanta <reginaldventura23@gmail.com>
2021-09-11 09:28:32 +0800

    Fix age and years of working

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  age: number = new Date().getFullYear() - 1993;
+  years_of_working: number = new Date().getFullYear() - 2014;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onGoToSection(element) {
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
   }
-
 }
