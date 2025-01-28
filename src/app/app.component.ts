4b16735d6fd25bd2ec6e1e31a709aa873e3bb03e
Reginald Mabanta <reginaldventura23@gmail.com>
2025-01-28 15:13:37 +0800

    add home feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,6 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: '<router-outlet></router-outlet>',
+  template: ` <router-outlet></router-outlet> `,
 })
-export class AppComponent {
-  title = 'portolio';
-}
+export class AppComponent {}
