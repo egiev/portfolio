666dcaf584c564bdca0cca4165ecb70a7ecfcfdf
Reginald Mabanta <reginaldventura23@gmail.com>
2025-02-18 13:15:07 +0800

    add brain animation

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -9,15 +9,21 @@ import {
 import gsap from 'gsap';
 import { fromEvent } from 'rxjs';
 import {
-  BoxGeometry,
+  BufferAttribute,
+  BufferGeometry,
+  Color,
+  Group,
   Mesh,
-  MeshBasicMaterial,
   Object3D,
   PerspectiveCamera,
+  Points,
+  PointsMaterial,
   Scene,
+  TextureLoader,
   WebGLRenderer,
 } from 'three';
 import { Timer } from 'three/addons/misc/Timer.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 @Component({
   selector: 'app-home',
@@ -33,19 +39,35 @@ export class HomeComponent implements OnInit {
   renderer!: WebGLRenderer;
   timer: Timer = new Timer();
 
-  mesh!: Object3D;
+  cameraGroup!: Object3D;
+  brain!: Object3D;
+  particles!: Points;
 
+  currentSection = 0;
   scrollY = window.scrollY;
+  cursor = { x: 0, y: 0 };
   objectDistance = 4;
 
   ngOnInit(): void {
+    this.init();
+    this.initModels();
+    this.initParticles();
+    this.registerListener();
+    this.zone.runOutsideAngular(() => this.tick());
+  }
+
+  private init() {
     // Scene
     this.scene = new Scene();
 
     // Camera
-    this.camera = new PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 100);
-    this.camera.position.set(0, 0, 5);
+    this.cameraGroup = new Group();
+    this.scene.add(this.cameraGroup);
+
+    this.camera = new PerspectiveCamera(35, innerWidth / innerHeight, 0.1, 100);
+    this.camera.position.set(0, 0, 6);
     this.camera.aspect = innerWidth / innerHeight;
+    this.cameraGroup.add(this.camera);
 
     // Renderer
     this.renderer = new WebGLRenderer({
@@ -55,30 +77,93 @@ export class HomeComponent implements OnInit {
     this.renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
     this.renderer.setSize(innerWidth, innerHeight);
     this.renderer.render(this.scene, this.camera);
-
-    this.initModels();
-    this.registerListener();
-    this.zone.runOutsideAngular(() => this.tick());
   }
 
-  private initModels() {
-    // Object
-    const geometry = new BoxGeometry(1, 1, 1);
-    const material = new MeshBasicMaterial();
-    this.mesh = new Mesh(geometry, material);
-    this.mesh.position.x = 2;
-    this.mesh.position.y = -this.objectDistance * 0;
-    this.scene.add(this.mesh);
+  private async initModels() {
+    const loader = new GLTFLoader();
+    const model = await loader.loadAsync('/models/human_brain.glb');
+    const positions: number[] = [];
+
+    model.scene.traverse((node) => {
+      if (node instanceof Mesh) {
+        node.geometry.center();
+        positions.push(...node.geometry.attributes.position.array);
+      }
+    });
+
+    const geometry = new BufferGeometry();
+    geometry.center();
+    geometry.setAttribute(
+      'position',
+      new BufferAttribute(new Float32Array(positions), 3),
+    );
+
+    const material = new PointsMaterial();
+    material.size = 0.3;
+    material.sizeAttenuation = true;
+    material.alphaMap = await this.loadTexture();
+    material.color = new Color('#df800d');
+    material.transparent = true;
+    material.depthWrite = false;
+
+    this.brain = new Points(geometry, material);
+    this.brain.scale.set(0.03, 0.03, 0.03);
+    this.brain.position.x = 1.5;
+    this.brain.position.y = -this.objectDistance * 0;
+    this.brain.rotation.y = Math.PI * 0.5 * 0.5;
+    this.scene.add(this.brain);
+  }
+
+  private async loadTexture() {
+    const textureLoader = new TextureLoader();
+    const particleTexture = await textureLoader.loadAsync('/textures/8.png');
+    return particleTexture;
+  }
+
+  private async initParticles() {
+    const count = 250;
+    const geometry = new BufferGeometry();
+
+    const positions = new Float32Array(count * 3);
+    for (let i = 0; i < count * 3; i += 3) {
+      positions[i] = (Math.random() - 0.5) * 10;
+      positions[i + 1] =
+        this.objectDistance * 0.7 - Math.random() * this.objectDistance * 4;
+      positions[i + 2] = (Math.random() - 0.5) * 10;
+    }
+
+    geometry.setAttribute('position', new BufferAttribute(positions, 3));
+
+    const material = new PointsMaterial();
+    material.size = 0.025;
+    material.sizeAttenuation = true;
+    material.color = new Color('#df800d');
+    // material.map = await this.loadTexture();
+    // material.transparent = true;
+    // material.depthWrite = false;
+
+    this.particles = new Points(geometry, material);
+    this.scene.add(this.particles);
   }
 
   private tick() {
     // Timer
     this.timer.update();
+    const delta = this.timer.getDelta();
 
     // Camera
     this.camera.position.y =
       (-this.scrollY / innerHeight) * this.objectDistance;
 
+    // Parallax
+    const parallaxX = this.cursor.x * 0.5;
+    const parallaxY = this.cursor.y * 0.5;
+    this.cameraGroup.position.x =
+      parallaxX - this.cameraGroup.position.x * 5 * delta;
+    this.cameraGroup.position.y =
+      parallaxY - this.cameraGroup.position.y * 5 * delta;
+
+    // Renderer
     this.renderer.render(this.scene, this.camera);
 
     requestAnimationFrame(() => this.tick());
@@ -87,21 +172,7 @@ export class HomeComponent implements OnInit {
   private registerListener() {
     this.resize();
     this.scroll();
-  }
-
-  private scroll() {
-    fromEvent(window, 'scroll').subscribe(() => {
-      this.scrollY = window.scrollY;
-
-      const currentSection = Math.floor(this.scrollY / innerHeight);
-
-      gsap.to(this.mesh.position, {
-        duration: 0.7,
-        y: -this.objectDistance * currentSection,
-        ease: 'power3.inOut',
-      });
-      console.log(currentSection);
-    });
+    this.mousemove();
   }
 
   private resize() {
@@ -111,6 +182,31 @@ export class HomeComponent implements OnInit {
 
       this.renderer.setSize(innerWidth, innerHeight);
       this.renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
+    });
+  }
+
+  private scroll() {
+    fromEvent(window, 'scroll').subscribe(() => {
+      this.scrollY = window.scrollY;
+
+      const newSection = Math.floor(this.scrollY / innerHeight);
+      if (newSection !== this.currentSection) {
+        this.currentSection = newSection;
+
+        gsap.to(this.brain.position, {
+          duration: 0.7,
+          y: -this.objectDistance * this.currentSection,
+          x: this.brain.position.x * -1,
+          ease: 'power3.inOut',
+        });
+      }
+    });
+  }
+
+  private mousemove() {
+    fromEvent(window, 'mousemove').subscribe((e) => {
+      this.cursor.x = (e as MouseEvent).clientX / innerWidth - 0.5;
+      this.cursor.y = -((e as MouseEvent).clientY / innerHeight - 0.5);
     });
   }
 }
